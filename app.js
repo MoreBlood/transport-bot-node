a8768fadb7e552562d3d1cfeb9f8ee566a9648d1
Kirill Artihovich <new.shtirlic@mail.ru>
2017-11-01 10:03:29 +0200

    Убрал express js, сделал базовую функциональсть (#1)

--- app.js
+++ app.js
@@ -1,49 +1,127 @@
-const express = require('express');
-const path = require('path');
-const logger = require('morgan');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
 const config = require('./lib/config');
-
-const index = require('./routes/index');
-const randomstring = require('randomstring');
 const client = require('./lib/telegram');
+const log = require('./lib/log')(module);
+const VK = require('node-vkapi');
 
-const WEBHOOK_TOKEN = randomstring.generate(16);
+const TRASH = ['еще', 'стоят', '\\!', '\\)', 'написать сообщение', 'собаки', 'опять', 'проверяют', 'не проверяют'];
+const BAD_WORDS = ['нет', 'нету', '\\?', 'никого', 'где', 'не обнаруж', 'чисто', 'есть кто', 'до', 'как', 'дармоеды', 'гады', 'ничего', 'давайте',
+  'будем', 'фоткать', 'народ', 'люди'];
+const LOAD_TIME = 60;
+const REFRESH_BUTTONS = {
+  inline_keyboard: [
+    [
+      {
+        text: 'Обновить',
+        callback_data: 'refresh_control',
+      },
+      {
+        text: 'Группа ВК',
+        url: 'https://vk.com/kontroler_brest',
+      },
+    ],
+  ],
+};
+const REFRESH_BUTTONS_BLOCK = {
+  reply_markup: REFRESH_BUTTONS,
+  parse_mode: 'Markdown',
+};
 
-const app = express();
+const findWordInSentence = (sentence) => {
+  for (let i = 0; i < BAD_WORDS.length; i += 1) {
+    if (new RegExp(BAD_WORDS[i], 'gi').test(sentence)) {
+      return false;
+    }
+  }
+  return true;
+};
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+const deleteTrash = (sentence) => {
+  for (let i = 0; i < TRASH.length; i += 1) {
+    sentence.replace(new RegExp(TRASH[i], 'gi'), '');
+  }
+  return sentence;
+};
 
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+const timeFormater = (time) => {
+  const hours = Math.floor(time / 60);
+  const minutes = time % 60;
+  return !hours && !minutes ? 'только что' : `${hours ? `${hours} час.` : ''}${hours && minutes ? ' ' : ''}${minutes ? `${minutes} мин.` : ''}`;
+};
 
-app.use('/', index);
-
-client.setWebhook(`${config.get('url')}/${WEBHOOK_TOKEN}`);
-app.use(`/${WEBHOOK_TOKEN}`, require('./routes/bot'));
-
-// catch 404 and forward to error handler
-app.use((req, res, next) => {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+const vk = new VK({
+  app: {
+    id: config.get('vk_id'),
+  },
 });
 
-// error handler
-app.use((err, req, res, next) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+client.openWebHook();
+client.setWebHook(`${process.env.URL || config.get('url')}/bot${config.get('telegram_bot_api_token')}`);
+
+const getControll = minutes => new Promise((resolve, reject) => {
+  let lastMessage;
+  vk.call('wall.get', { access_token: config.get('vk_secret'), owner_id: -72869598, filter: 'others', count: 100 })
+    .then((response) => {
+      resolve(response.items
+        .map(elem => ({
+          text: elem.text,
+          time: Math.round((Date.now().toString().slice(this.length, -3) - elem.date) / 60),
+        }))
+        .filter(elem => findWordInSentence(elem.text))
+        .map((elem) => {
+          lastMessage = lastMessage || elem;
+          return elem;
+        })
+        .filter(elem => elem.time < minutes || LOAD_TIME)
+        .map(elem => `${deleteTrash(elem.text)} *(${timeFormater(elem.time)})*`)
+        .join('\n') || `За *${timeFormater(LOAD_TIME)}* не было замечено контроля${lastMessage ? `\nПоследнее сообщение *${timeFormater(lastMessage.time)} назад*:\n${lastMessage.text}` : ''}`);
+    })
+    .catch(err => reject(err));
 });
 
-module.exports = app;
+client.onText(/\/control/g, (msg) => {
+  const id = msg.chat.id;
+  getControll()
+    .then(res => client.sendMessage(id, res, REFRESH_BUTTONS_BLOCK))
+    .catch(err => log.error(err));
+});
+
+client.onText(/\/start/g, (msg) => {
+  const id = msg.chat.id;
+  client.sendMessage(id, 'Чтобы узнать где контролеры отправь /control либо жми на кнопку', {
+    parse_mode: 'Markdown',
+    reply_markup: {
+      inline_keyboard: [
+        [
+          {
+            text: 'Где контроллеры?',
+            callback_data: 'control',
+          },
+        ],
+      ],
+    },
+  })
+    .catch(err => log.error(err.message));
+});
+
+client.on('callback_query', (callbackQuery) => {
+  const action = callbackQuery.data;
+  const msg = callbackQuery.message;
+  const opts = {
+    chat_id: msg.chat.id,
+    message_id: msg.message_id,
+    reply_markup: REFRESH_BUTTONS,
+    parse_mode: 'Markdown',
+  };
+  if (action === 'control') {
+    getControll()
+      .then(res => client.sendMessage(msg.chat.id, res, REFRESH_BUTTONS_BLOCK))
+      .catch(err => log.error(err.message));
+  }
+  if (action === 'refresh_control') {
+    getControll()
+      .then(res => client.editMessageText(res, opts))
+      .catch(err => log.error(err.message));
+  }
+});
+
